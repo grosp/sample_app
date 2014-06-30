8580da8e84a6d076f1643d5fffd7144f139b793e
Paul Grossman <grosp@mail.med.upenn.edu>
2014-06-30 07:44:28 -0400

    Finish layout and routes

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require File.expand_path('../boot', __FILE__)
+
 module SampleApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -24,5 +26,8 @@ module SampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
   end
 end
